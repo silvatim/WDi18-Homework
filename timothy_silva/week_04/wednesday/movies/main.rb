72d7b1f93bb8fc65015af582f66a8b1d575aa398
silvatim <silva.tim@gmail.com>
2016-11-17 09:36:35 +1100

    removedroutes in main.rb

--- timothy_silva/week_04/wednesday/movies/main.rb
+++ timothy_silva/week_04/wednesday/movies/main.rb
@@ -16,28 +16,3 @@ end
 get "/search" do
    erb :search
 end
-
-# get "/auto/:color" do
-#    @color = params[:color]
-#    erb :car_color#"dynamic contact to be loaded
-# end
-#
-# response = HTTParty.get "http://www.omdbapi.com/?t=Satantango"
-#
-# get "/users" do
-#   @first = "Larry"
-#   @second = "Barry"
-#   @third = "Garry"
-#   erb :users_index
-# end
-#
-# get "/users/:username" do
-#   @name = params[:username]
-#   erb :users_show
-# end
-#
-# get "/results" do
-#   @first_num = params[:first_num].to_f
-#   @second_num = params[:second_num].to_f
-#   erb :results
-# end
